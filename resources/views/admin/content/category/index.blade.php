e731ba3e5d0d87618cce97e19b22cfc9fb4b8c1f
Abolfazl <kabolfazl39@gmail.com>
2021-10-24 21:25:36 +0330

    complate post category index

--- resources/views/admin/content/category/index.blade.php
+++ resources/views/admin/content/category/index.blade.php
@@ -8,14 +8,14 @@
 
 <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
-      <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
-      <li class="breadcrumb-item font-size-12"> <a href="#">بخش فروش</a></li>
-      <li class="breadcrumb-item font-size-12 active" aria-current="page"> دسته بندی</li>
+        <li class="breadcrumb-item font-size-12"><a href="#">خانه</a></li>
+        <li class="breadcrumb-item font-size-12"><a href="#">بخش محتوا</a></li>
+        <li class="breadcrumb-item font-size-12 active" aria-current="page"> دسته بندی</li>
     </ol>
-  </nav>
+</nav>
 
 
-  <section class="row">
+<section class="row">
     <section class="col-12">
         <section class="main-body-container">
             <section class="main-body-container-header">
@@ -34,41 +34,52 @@
             <section class="table-responsive">
                 <table class="table table-striped table-hover">
                     <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>نام دسته بندی</th>
-                            <th>دسته والد</th>
-                            <th class="max-width-16-rem text-center"><i class="fa fa-cogs"></i> تنظیمات</th>
-                        </tr>
+                    <tr>
+                        <th>#</th>
+                        <th>نام دسته بندی</th>
+                        <th>توضیحات</th>
+                        <th>اسلاگ</th>
+                        <th>عکس</th>
+                        <th>تگ ها</th>
+                        <th>وضعیت</th>
+                        <th class="max-width-16-rem text-center"><i class="fa fa-cogs"></i> تنظیمات</th>
+                    </tr>
                     </thead>
                     <tbody>
+                    @foreach($postCategories as $postCategory)
                         <tr>
-                            <th>1</th>
-                            <td>نمایشگر	</td>
-                            <td>کالای الکترونیکی</td>
+                            <th>#</th>
+                            <th>{{ $postCategory->name }}</th>
+                            <th>{{ $postCategory->description }}</th>
+                            <th>{{ $postCategory->slug }}</th>
+                            <th>
+                                <img src="{{ asset($postCategory->image) }}" width="50" height="50">
+                            </th>
+                            <th>{{ $postCategory->tags }}</th>
+                            <th>
+                                <label>
+                                    <input type="checkbox" @if ($postCategory->status === 1) checked @endif>
+                                </label>
+                            </th>
+
                             <td class="width-16-rem text-left">
-                                <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
-                                <button class="btn btn-danger btn-sm" type="submit"><i class="fa fa-trash-alt"></i> حذف</button>
+                                <a href="{{ route('admin.content.category.edit',$postCategory->id) }}"
+                                   class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
+                                <form class="d-inline"
+                                      action="{{ route('admin.content.category.destroy',$postCategory->id) }}"
+                                      method="post">
+                                    @csrf
+                                    @method('delete')
+                                    <button class="btn btn-danger btn-sm" type="submit"><i class="fa fa-trash-alt"></i>
+                                        حذف
+                                    </button>
+                                </form>
+
                             </td>
                         </tr>
-                        <tr>
-                            <th>2</th>
-                            <td>نمایشگر	</td>
-                            <td>کالای الکترونیکی</td>
-                            <td class="width-16-rem text-left">
-                                <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
-                                <button class="btn btn-danger btn-sm" type="submit"><i class="fa fa-trash-alt"></i> حذف</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>3</th>
-                            <td>نمایشگر	</td>
-                            <td>کالای الکترونیکی</td>
-                            <td class="width-16-rem text-left">
-                                <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
-                                <button class="btn btn-danger btn-sm" type="submit"><i class="fa fa-trash-alt"></i> حذف</button>
-                            </td>
-                        </tr>
+                    @endforeach
+
+
                     </tbody>
                 </table>
             </section>
